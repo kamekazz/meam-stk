77baa67f08b21dbef572dc23809cac23e2a89dcf
manuel taveras <taveras78@gmail.com>
2018-06-12 13:09:57 -0400

    vdone

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// Components
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const {mongoose} = require('./database')
 const app = express();
 const portURL = process.env.PORT || 3000
@@ -9,6 +11,7 @@ const portURL = process.env.PORT || 3000
 // middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
 // route
 app.use('/api/employees',require('./routes/employee'))
 
